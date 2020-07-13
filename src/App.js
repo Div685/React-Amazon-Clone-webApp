2e2b12777409b75f6a5a1d73127e0563f1380268
Div <divyeshpatel685@gmail.com>
2020-07-14 00:17:19 +0300

    First Commit to set up files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Header from "./header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/">
+            <Header />
+            <h1>Home Page</h1>
+          </Route>
+          <Route path="/login"></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
