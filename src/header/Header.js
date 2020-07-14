9fcdd0d4c247964ad6bcbc5ef985349d0ec37a9b
Div <divyeshpatel685@gmail.com>
2020-07-14 23:55:32 +0300

    add header css

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <nav className="header"></nav>;
+  return (
+    <nav className="header">
+      <Link to="/">
+        <img
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          className="header__logo"
+          alt="logo"
+        />
+      </Link>
+      <input type="text" className="header__searchInput" value="" />
+    </nav>
+  );
 }
 
 export default Header;
